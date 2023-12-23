1b9da6f10d6fd950efa8a68f1a314062a65d88f0
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-23 08:29:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function activateSearch() {
+	const search1 = document.getElementsByClassName("search");
+	const active1 = document.getElementsByClassName("input");
+	search1.classList.toggle("active");
+	 if (search1.classList.contains("active")) {
+            active1.focus();
+        }
+}
